0ce6114f828639c84d04df4e3ddb50d39dadd015
JeanNicolasdeLamballerie <55099344+JeanNicolasdeLamballerie@users.noreply.github.com>
2025-03-10 18:08:05 +0100

    feat: adds basic, configurable logger (with file).

--- src/logger.rs
+++ src/logger.rs
@@ -1,30 +1,66 @@
-use log::{debug, error, info, trace, warn};
-use std::time::SystemTime;
+// use log::{debug, error, info, trace, warn};
+use std::{env, time::SystemTime};
 
-fn setup_logger() -> Result<(), fern::InitError> {
+#[cfg(debug_assertions)]
+fn use_level() -> log::LevelFilter {
+    if let Ok(level) = env::var("OXYDANT_LOG_LEVEL") {
+        let envlevel: &str = &level.to_uppercase();
+        match envlevel {
+            "TRACE" => log::LevelFilter::Trace,
+            "DEBUG" => log::LevelFilter::Debug,
+            "INFO" => log::LevelFilter::Info,
+            "ERROR" => log::LevelFilter::Error,
+            "WARN" => log::LevelFilter::Warn,
+            "OFF" => log::LevelFilter::Off,
+            _ => log::LevelFilter::Debug,
+        }
+    } else {
+        log::LevelFilter::Debug
+    }
+}
+#[cfg(not(debug_assertions))]
+fn use_level() -> log::LevelFilter {
+    if let Ok(level) = env::var("OXYDANT_LOG_LEVEL") {
+        let envlevel: &str = &level.to_uppercase();
+        match envlevel {
+            "TRACE" => log::LevelFilter::Trace,
+            "DEBUG" => log::LevelFilter::Debug,
+            "INFO" => log::LevelFilter::Info,
+            "ERROR" => log::LevelFilter::Error,
+            "WARN" => log::LevelFilter::Warn,
+            "OFF" => log::LevelFilter::Off,
+            _ => log::LevelFilter::Debug,
+        }
+    } else {
+        log::LevelFilter::Error
+    }
+}
+pub fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
                 "[{} {} {}] {}",
-                humantime::format_rfc3339_seconds(SystemTime::now()),
+                humantime::format_rfc3339_micros(SystemTime::now()),
                 record.level(),
                 record.target(),
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(use_level())
         .chain(std::io::stdout())
-        .chain(fern::log_file("output.log")?)
+        .chain(
+            fern::Dispatch::new()
+                .format(|out, message, record| {
+                    out.finish(format_args!(
+                        "[{} {} {}] {}",
+                        humantime::format_rfc3339_micros(SystemTime::now()),
+                        record.level(),
+                        record.target(),
+                        message
+                    ))
+                })
+                .chain(fern::log_file("output.log")?),
+        )
         .apply()?;
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    setup_logger()?;
-
-    info!("Hello, world!");
-    warn!("Warning!");
-    debug!("Now exiting.");
-
     Ok(())
 }
